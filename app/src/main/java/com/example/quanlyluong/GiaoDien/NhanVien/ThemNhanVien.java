99f5fb8abeaeb21f95da2c3b5bb5ebf8d063405c
tranbinhan2002 <tranbinhan20021997@gmail.com>
2020-07-23 11:05:59 +0700

    Update project 25/07/2020 1.0.2

--- app/src/main/java/com/example/quanlyluong/GiaoDien/NhanVien/ThemNhanVien.java
+++ app/src/main/java/com/example/quanlyluong/GiaoDien/NhanVien/ThemNhanVien.java
@@ -13,6 +13,7 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.quanlyluong.Model.NhanVien;
 import com.example.quanlyluong.R;
 
 import java.util.ArrayList;
@@ -23,7 +24,7 @@ public class ThemNhanVien extends AppCompatActivity {
     RadioButton radNam,radNu;
     Spinner spPhongBan;
     ArrayList<String> data_phongban = new ArrayList<>();
-    ArrayAdapter adapter_PhongBan;
+    ArrayAdapter adapter_phongban;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,12 +35,27 @@ public class ThemNhanVien extends AppCompatActivity {
 
     private void setEvent() {
         LoadPhongBan();
-        adapter_PhongBan = new ArrayAdapter(ThemNhanVien.this,android.R.layout.simple_spinner_item,data_phongban);
-        spPhongBan.setAdapter(adapter_PhongBan);
+        adapter_phongban = new ArrayAdapter(ThemNhanVien.this,android.R.layout.simple_spinner_item,data_phongban);
+        spPhongBan.setAdapter(adapter_phongban);
 
         btnLuuNhanVien.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                NhanVien nhanVien = new NhanVien();
+                nhanVien.setMaNhanVien(txtMaNhanVien.getText().toString());
+                nhanVien.setTenNhanVien(txtTenNhanVien.getText().toString());
+                nhanVien.setNgaySinh(txtNgaySinh.getText().toString());
+                if(radNam.isChecked()==true)
+                {
+                    nhanVien.setGioiTinh("Nam");
+                }
+                if(radNu.isChecked()==true)
+                {
+                     nhanVien.setGioiTinh("Nữ");
+                }
+                nhanVien.setPhongBan(spPhongBan.getSelectedItem().toString());
+                nhanVien.setHeSoLuong(txtHeSoLuong.getText().toString());
+                
                 Intent intent =new Intent(ThemNhanVien.this,MainNhanVien.class);
                 startActivity(intent);
             }
@@ -52,5 +68,13 @@ public class ThemNhanVien extends AppCompatActivity {
     }
     private void setControl() {
         btnLuuNhanVien = findViewById(R.id.btnLuuNhanVien);
+        spPhongBan = findViewById(R.id.spPhongBan);
+        txtMaNhanVien = findViewById(R.id.txtMaNhanVien);
+        txtTenNhanVien = findViewById(R.id.txtTenNhanVien);
+        txtNgaySinh = findViewById(R.id.txtNgaySinh);
+        radNam = findViewById(R.id.radNam);
+        radNu = findViewById(R.id.radNu);
+        txtHeSoLuong= findViewById(R.id.txtHeSoLuong);
+
     }
 }
