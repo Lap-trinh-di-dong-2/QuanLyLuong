9929676e5129c28ecab7c6aea8702ef60eb15a72
tranbinhan2002 <tranbinhan20021997@gmail.com>
2020-07-25 14:26:19 +0700

    Update project 25/07/2020 1.0.4

--- app/src/main/java/com/example/quanlyluong/GiaoDien/GioiThieu.java
+++ app/src/main/java/com/example/quanlyluong/GiaoDien/GioiThieu.java
@@ -10,12 +10,14 @@ import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.quanlyluong.R;
 
 public class GioiThieu extends AppCompatActivity {
     ImageView imgChuyenDong,imgTien1,imgTien2,imgTien3,imgTien4,imgTien5;
-    Animation aniHien,aniHien1,aniHien2,aniHien3,aniHien4;
+    Animation aniHien,aniHien1,aniHien2,aniHien3,aniHien4,aniQuan,aniLy,aniLuong;
+    TextView tvQuan,tvLy,tvLuong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,11 +43,17 @@ public class GioiThieu extends AppCompatActivity {
         aniHien2 = AnimationUtils.loadAnimation(this,R.anim.hienlen2);
         aniHien3 = AnimationUtils.loadAnimation(this,R.anim.hienlen3);
         aniHien4 = AnimationUtils.loadAnimation(this,R.anim.hienlen4);
+        aniQuan = AnimationUtils.loadAnimation(this,R.anim.quan);
+        aniLy = AnimationUtils.loadAnimation(this,R.anim.ly);
+        aniLuong = AnimationUtils.loadAnimation(this,R.anim.luong);
         imgTien1.startAnimation(aniHien);
         imgTien2.startAnimation(aniHien1);
         imgTien3.startAnimation(aniHien2);
         imgTien4.startAnimation(aniHien3);
         imgTien5.startAnimation(aniHien4);
+        tvQuan.startAnimation(aniQuan);
+        tvLy.startAnimation(aniLy);
+        tvLuong.startAnimation(aniLuong);
     }
 
     private void setControl() {
@@ -54,6 +62,9 @@ public class GioiThieu extends AppCompatActivity {
         imgTien3=findViewById(R.id.imgTien3);
         imgTien4=findViewById(R.id.imgTien4);
         imgTien5=findViewById(R.id.imgTien5);
+        tvQuan = findViewById(R.id.tvQuan);
+        tvLy = findViewById(R.id.tvLy);
+        tvLuong = findViewById(R.id.tvLuong);
 //        imgChuyenDong = findViewById(R.id.imgChuyenDong);
     }
 }
